0664b3b1a8e4bfc70184d8566fdc78c6aa73ad93
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-04-14 22:11:18 +0300

    Added admin model.

---
+++ models/admin.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+    name: String,
+    surname: String,
+    password: String,
+    userName: String
+});
+
+module.exports = mongoose.model('Admin',adminSchema);
